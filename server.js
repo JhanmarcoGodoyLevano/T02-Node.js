e960d45280953249448084450cf08659d6b164af
Jhanmarco Godoy Levano <111781171+JhanmarcoGodoyLevano@users.noreply.github.com>
2022-12-13 06:54:09 -0500

    cambio2

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello world with express'))
+app.get('/', (req, res) => {
+    res.sendFile('/home/ubuntu/projects2/T02-Node.js/index.html')});
 
 app.listen(3000);
 console.log('Server on port 3000')
